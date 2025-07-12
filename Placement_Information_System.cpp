07cd206cf6a0a2a0535e607c96b5914a04f131d2
Bhavin-0 <bhavin.joshi@mitaoe.ac.in>
2025-07-12 12:34:02 +0530

    added functionalities

--- Placement_Information_System.cpp
+++ Placement_Information_System.cpp
@@ -21,7 +21,7 @@ class PlacementSystem{
         PlacementSystem();
         void insertRecord();
         void updateRecord();
-        void serachRecord();
+        void searchRecord();
         void sortRecord();
         void listRecord();
         void deleteRecord();
@@ -57,6 +57,134 @@ void PlacementSystem::insertRecord(){
 
     }
     cout<<"Record Inserted Successfully.";
+}
+
+void PlacementSystem::updateRecord(){
+    if(head == nullptr){
+        cout<<"\nNo Records to Update.";
+    }
+    int updateId;
+    cout<<"Enter the PRN : ";
+    cin>>updateId;
+    PlacementRecord* temp = head;
+    while(temp != nullptr){
+        if(temp->prn == updateId){
+            cout<<"\n========== RECORD FOUND OF "<<temp->prn<<" ==========";
+            cout << "Current Name: " << temp->name << endl;
+            cout << "Current Branch: " << temp->branch << endl;
+            cout << "Current Percentage: " << temp->cgpa << endl;
+            cout << "Current Company Name: " << temp->companyName << endl;
+
+            cout<<"\nEnter New Name : ";
+            cin>>temp->name;
+
+            cout<<"Enter New Branch : ";
+            cin>>temp->branch;
+
+            cout<<"Enter New CGPA : ";
+            cin>>temp->cgpa;
+
+            cout<<"Enter New Company Name : ";
+            cin>>temp->companyName;
+
+            cout<<"Record Updated Successfully.";
+            return;
+        }
+        temp = temp->next;
+    }
+
+    cout<<"Record with "<<temp->prn<<" not found !!";
+}
+
+void PlacementSystem::listRecord(){
+    PlacementRecord* temp = head;
+    int count=1;
+    cout<<"\n========== LIST OF PLACEMENT RECORDS ==========";
+    while (temp != nullptr)
+    {
+        cout<<"\n Record # "<<count++;
+        cout<<"PRN : "<<temp->prn<<endl;
+        cout<<"Name : "<<temp->name<<endl;
+        cout<<"branch : "<<temp->branch<<endl;
+        cout<<"CGPA : "<<temp->cgpa<<endl;
+        cout<<"Company Name : "<<temp->companyName<<endl;
+        cout << "-------------------------------\n";
+
+        temp = temp->next;
+    }
+    
+
+}
+
+void PlacementSystem::deleteRecord(){
+    if(head == nullptr){
+        cout<<"\nNo Records to delete!! ";
+        return;
+    }
+
+    int deleteId;
+    cout<<"Enter PRN to delete : ";
+    cin>>deleteId;
+    PlacementRecord* temp = head;
+    PlacementRecord* prev = nullptr;
+    //case 1: delete head node 
+    if(temp->prn == deleteId){
+        head = head->next;
+        delete temp;
+        cout<<"\nRecord deleted Successfully.";
+        return;
+    }
+    //case 2: To Traverse the list of nodes
+    while(temp != nullptr && temp->prn != deleteId){
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if(temp == nullptr) {
+        cout << "\nRecord deleted successfully for PRN: " << deleteId;
+        return;
+    }
+    
+    //case 3: if node found  
+    prev->next = temp->next;
+    delete temp;
+    cout<<"\nRecord Successfully deleted. ";
+}
+
+void PlacementSystem::searchRecord(){
+    if(head == nullptr){
+        cout<<"\nNo Records to search.";
+        return;
+    }
+
+    bool found = false;
+    int searchId;
+    cout<<"\nEnter PRN to search : ";
+    cin>>searchId;
+
+    PlacementRecord* temp = head;
+
+    while(temp != nullptr){
+        if(temp->prn == searchId){
+
+            found = true;
+            cout<<"\n--------- Searched Record --------- ";
+            cout<<"PRN : "<<temp->prn<<endl;
+            cout<<"Name : "<<temp->name<<endl;
+            cout<<"branch : "<<temp->branch<<endl;
+            cout<<"CGPA : "<<temp->cgpa<<endl;
+            cout<<"Company Name : "<<temp->companyName<<endl;
+            cout << "-------------------------------\n";
+
+            cout<<"\nRecord Found. ";
+            break;
+        }
+        temp = temp->next;
+    }
+
+    if(!found){
+        cout<<"\nRecord Not Found !!.";
+    }
 }
 
 int main(){
